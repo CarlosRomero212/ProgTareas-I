4825e4803e8fb942435f5ee4a5bbb4dde36e2995
CARLOS ARNOLDO ROMERO ESPINAL <157551786+CarlosRomero212@users.noreply.github.com>
2024-03-05 21:20:09 -0600

    Proyecto APP COMPRA

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+        tbh.addTab(tbh.newTabSpec("Inicio").setIndicator("Inicio", null).setContent(R.id.Inicio));
+        tbh.addTab(tbh.newTabSpec("Gestion").setIndicator("Gestion", null).setContent(R.id.Gestion));
     }
 }
